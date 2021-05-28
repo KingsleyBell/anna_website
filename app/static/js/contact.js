a3736ea3db634f756f4ac48147e58f37392b6693
Luke Bell <lukekingsleybell@gmail.com>
2021-05-28 16:25:48 +0200

    Add edit contact admin button.

--- app/static/js/contact.js
+++ app/static/js/contact.js
@@ -17,11 +17,8 @@ $(document).ready(function () {
     quill.clipboard.dangerouslyPasteHTML(htmlDecode(contactText));
 
     $("#contact-form").on("submit", function () {
-        var myHeadingEditor = document.querySelector("#snow-container-heading");
         var myTextEditor = document.querySelector("#snow-container-text");
-        var headingHtml = myHeadingEditor.children[0].innerHTML;
         var textHtml = myTextEditor.children[0].innerHTML;
-        $("#heading").val(headingHtml);
         $("#text").val(textHtml);
     });
 });
